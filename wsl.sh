01a6570ebb1219d5506f16733e9a8dc19540805c
Richard Shade <richard@pulumi.com>
2022-04-27 10:50:00 -0500

    Initial Push

---
+++ wsl.sh
@@ -0,0 +1,27 @@
+    1  sudo -i su
+    2  ln -nsf /mnt/c/Github/ github
+    3  cd github
+    4  ls
+    5  cd pulumi-workstation-setup/
+    6  code .
+    7  ssh-keygen -t rsa
+    8  git config
+    9  git config -l
+   10  git config --global user.name "Richard Shade"
+   11  git config --global user.email richard@pulumi.com
+   12  sudo apt-get install gpa seahorse
+   13  sudo add-apt-repository ppa:longsleep/golang-backports
+   14  sudo apt install golang-1.18
+   15  history > wsl.sh
+   16  code code --install-extension
+   17  code --install-extension
+   18  code --install-extension --help
+   19  code --install-extension @recommended
+   20  code .
+   21  apt install nodejs dotnet 
+   22  sudo apt install nodejs dotnet 
+   23  wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+   24  sudo dpkg -i packages-microsoft-prod.deb
+   25  rm packages-microsoft-prod.deb
+   26  sudo apt-get update;   sudo apt-get install -y apt-transport-https &&   sudo apt-get update &&   sudo apt-get install -y dotnet-sdk-6.0
+   27  history > wsl.sh
